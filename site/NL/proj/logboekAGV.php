ee220676720d58098127e3fd612025d944fb80c3
JanHav <haverbekej@gmail.com>
2018-03-25 20:42:47 +0200

    Maken logboek AGV pagina in NE gedeelte sit + maken content

---
+++ site/NL/proj/logboekAGV.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang=nl>
+<!--Bewerkt door Jan Haverbeke-->
+<head>
+	<meta charset="utf-8" />
+	<meta name= description content="Logboek AGV, op deze pagina kan je de vooruitgang van ons project volgen.">
+	<meta name= keywords content="AGV, vooruitgangn">
+	<title>GSMslot</title>
+	<link rel="icon" href="../../images/favicon.ico" type="image/ico" sizes="16x16">
+
+  <!-- Set the viewport width to device width for mobile -->
+  <meta name="viewport" content="width=device-width" />
+
+  <!-- Included CSS Files -->
+  <link rel="stylesheet" href="../../css/foundation.css">
+  <link rel="stylesheet" href="../../css/responsive.css">
+
+  <!--Included javascript files-->
+  <script src="../../js/modernizr.foundation.js"></script>
+
+  
+  </head>
+
+  <body>	
+	
+    <!--Header includen-->
+    <?php include("header.html"); ?>
+
+    <!--Uitleg over UWB-->
+	<div class="row">
+		<div class="twelve columns">
+			<div class="row">
+				<div class="twelve columns">
+					<h1>Logboek AGV</h1>
+				<p>
+
+					<br /><br />
+					Op deze pagina kan je de vooruitgang volgen van een groep studenten (Thomas, Arnout, Aurélien en Jan) die aan hun bachelorproef werken.
+					Elke week posten we kort wat we die week gedaan/ bereikt hebben.
+					<br /><br />
+
+					<h4>Week 1 (12-18 maart 2018)</h4>
+					<ul> <!-- <ul> gebruik je voor een ongeordende lijst en <ol> gebruik je voor een geordende lijst-->
+					  Ons team heeft vergadert om de doelstellingen tegen de tussentijdse voorstelling te bepalen. Voorts hebben:
+					  <li>Jan en Thomas hebben gezocht naar een navigatie-algoritme.</li>
+					  <li>Jan is nu begonnen met een pilot-navigatie versie te schrijven.</li>
+					  <li>Aurélien is bezig geweest met het 3D tekenen van verschillende componenten en een houder voor onze 'Arduino-stacks'.</li>
+					  <li>Arnout heeft de mogelijkheid van RC-bediening voor de AGV's onderzocht en de benodigde onderdelen laten bestellen. </li>
+					  <li>Arnout en Thomas hebben ook nog een paar sensoren verder bestudeert (aansluitingen, data binnenlezen, op de CAN bus plaatsen).</li>
+					</li>
+					</ul>  
+				</p>       
+										
+				</div>
+			</div>
+		</div>
+	</div>
+
+  <!--Footer includen-->
+  <?php include("footer.html"); ?>
+</body>
+
+
+
